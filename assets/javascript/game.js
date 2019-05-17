b6b7c3140edc52b30e5e4c25c9b61b7950bac415
Email Update <haojinzhao@yahoo.com>
2019-05-17 13:40:49 -0500

    change html, add myCharBox . change imgCharBox process

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,69 +13,71 @@ var defenderId,fighterId = "";
 var gameOverFlg = false;
 var enemyNum = 0;
 var defenderNum = 0;
+var fightBaseAp = 0;
 const maxEnemyNum = 3;
+
 //define array, 1:character id, 2:health points, 3:  attack power
 // 4: Counter Attack Power 5: base Attack Power
 //charInitial for the initial value, never change, charUpdate for updated value
-var charInitial = [["imgCap",120,8,8,"Captain",8],
-            ["imgDar",100,9,10,"Darth Vader",9],
-            ["imgMace",150,7,7,"Mace",7],
-            ["imgYoda",180,6,7,"Yoda",6]];
-var charUpdate = [["imgCap",120,8,8,"Captain",8],
+var charInitial = [["imgCap",120,5,8,"Captain",5],
             ["imgDar",100,3,7,"Darth Vader",3],
-            ["imgMace",150,7,7,"Mace",7],
-            ["imgYoda",180,6,10,"Yoda",10]]
+            ["imgMace",150,4,7,"Mace",4],
+            ["imgYoda",180,1,6,"Yoda",1]];
+var charUpdate = [["imgCap",120,5,8,"Captain",5],
+            ["imgDar",100,3,7,"Darth Vader",3],
+            ["imgMace",150,4,7,"Mace",4],
+            ["imgYoda",180,1,6,"Yoda",1]];
 
 $(document).ready(function() {
   
 
-
+//the main logic, process all the click on characters under my character, enemies and defender
 $(".imgCharacter").on("click",function(){
 
 
-    imgId = $(this).attr("id");
-    console.log("parent",$(this).parent("div"));
-    console.log("parent",$(this).parent());
+    imgId = $(this).attr("value");
     
+    //check the character's parent div to decide to do select my character or defender function
     if ($(this).parent("div").attr("id") === "charBox"){
         selectCharFun();
-    } else if ($(this).parent("div").attr("id") === "enemyBox"){
-        console.log("enemy");
-        selectEmyFun();
+    } else if ($(this).parent("div").attr("id") === "enemyBox"){        
+        selectDefFun();
     } 
-    //if select the character in the defender box, will do nothing.
+    //if select the character in the defender or my character box, will do nothing.
 })
 
 function selectCharFun(){
-
-    console.log("selectfun",imgId, firstSelectFlg);
+   
     if (!firstSelectFlg){
         switch(imgId){
-            case "imgCap":           
-                console.log("imgcap")  ;    
-                $(".imgCharBox2").detach().appendTo("#enemyBox");
-                $(".imgCharBox3").detach().appendTo("#enemyBox");
-                $(".imgCharBox4").detach().appendTo("#enemyBox");     
-                $('#your_modal_id').clone().prop("id", "new_modal_id").appendTo("target_container");         
+            case "imgCap":                        
+                $(".imgCharBox1").clone(true,true).appendTo("#myCharBox");
+                $(".imgCharBox2").clone(true,true).appendTo("#enemyBox");
+                $(".imgCharBox3").clone(true,true).appendTo("#enemyBox");
+                $(".imgCharBox4").clone(true,true).appendTo("#enemyBox");     
+                // clone( [withDataAndEvents] [, deepWithDataAndEvents] ), use true to add event 
                 break;
-             case "imgDar":            
-                $(".imgCharBox1").detach().appendTo("#enemyBox");
-                $(".imgCharBox3").detach().appendTo("#enemyBox");
-                $(".imgCharBox4").detach().appendTo("#enemyBox");                 
+             case "imgDar":      
+                $(".imgCharBox2").clone(true,true).appendTo("#myCharBox");      
+                $(".imgCharBox1").clone(true,true).appendTo("#enemyBox");
+                $(".imgCharBox3").clone(true,true).appendTo("#enemyBox");
+                $(".imgCharBox4").clone(true,true).appendTo("#enemyBox");                 
                 break;
             case "imgMace": 
-                $(".imgCharBox1").detach().appendTo("#enemyBox");
-                $(".imgCharBox2").detach().appendTo("#enemyBox");
-                $(".imgCharBox4").detach().appendTo("#enemyBox");
+                $(".imgCharBox3").clone(true,true).appendTo("#myCharBox");
+                $(".imgCharBox1").clone(true,true).appendTo("#enemyBox");
+                $(".imgCharBox2").clone(true,true).appendTo("#enemyBox");
+                $(".imgCharBox4").clone(true,true).appendTo("#enemyBox");
                 break;
             case "imgYoda": 
-                $(".imgCharBox1").detach().appendTo("#enemyBox");
-                $(".imgCharBox2").detach().appendTo("#enemyBox");
-                $(".imgCharBox3").detach().appendTo("#enemyBox");
+                $(".imgCharBox4").clone(true,true).appendTo("#myCharBox");
+                $(".imgCharBox1").clone(true,true).appendTo("#enemyBox");
+                $(".imgCharBox2").clone(true,true).appendTo("#enemyBox");
+                $(".imgCharBox3").clone(true,true).appendTo("#enemyBox");
                 break;
         }
-        enemyNum += maxEnemyNum;
-        console.log("enemy number ",enemyNum);
+        enemyNum += maxEnemyNum;      
+        $("#charBox").hide();
     }
                             
         $("#enemyBox").children().css({"border-color":"red"});
@@ -83,30 +85,28 @@ function selectCharFun(){
         firstSelectFlg = true;
 }
 
-function selectEmyFun(){
+function selectDefFun(){
     if (selectDefFlag){
         switch(imgId){
-            case "imgCap":           
-                console.log("imgcap")  ;    
-                $(".imgCharBox1").detach().appendTo("#defenderBox");    
+            case "imgCap":  
+                $("#enemyBox").find(".imgCharBox1").detach().appendTo("#defenderBox");    
                 selectDefFlag = false;                   
                 break;
             case "imgDar":            
-                $(".imgCharBox2").detach().appendTo("#defenderBox");     
+                $("#enemyBox").find(".imgCharBox2").detach().appendTo("#defenderBox");     
                 selectDefFlag = false;                                 
                 break;
             case "imgMace":         
-                $(".imgCharBox3").detach().appendTo("#defenderBox");  
+                $("#enemyBox").find(".imgCharBox3").detach().appendTo("#defenderBox");  
                 selectDefFlag = false;        
                 break;
             case "imgYoda": 
-                $(".imgCharBox4").detach().appendTo("#defenderBox");    
+                $("#enemyBox").find(".imgCharBox4").detach().appendTo("#defenderBox");    
                 selectDefFlag = false;                  
                 break;
         }
         $("#defenderBox").children().css({"border-color":"black"});
-        enemyNum--;
-        console.log("enemy number second ",enemyNum);
+        enemyNum--;       
         defenderNum++;
         $("#attackMsg").find("span").text("");
        $("#attackMsg").find("p").text("");
@@ -114,56 +114,43 @@ function selectEmyFun(){
     
 }
 
+//attack button onclick process
 $(".attackBtn").on("click",function(){
 
     attackText="";
     conAttackText="";   
-
+    fightBaseAp =0;
+    fighterAttP = 0;
+    defenderConAtt = 0;
+    
+//if not game over and there is  defender, do attack process
    if ((!gameOverFlg)&&(defenderNum>0)){       
         
-        defenderId = $("#defenderBox").find("div").attr("id");
-        fighterId = $("#charBox").find("div").attr("id");
+        defenderId = $("#defenderBox").find("div").attr("value");
+        fighterId = $("#myCharBox").find("div").attr("value");
 
         for (i=0;i<charUpdate.length;i++){
-            console.log("array",charUpdate[i]);
+         
             if (charUpdate[i][0] === defenderId){
                 defenderHp = charUpdate[i][1];
                 defenderConAtt = charUpdate[i][3];
                 defenderName = charUpdate[i][4];
+               
             } else if (charUpdate[i][0] === fighterId){
                 fighterHp = charUpdate[i][1];
                 fighterAttP = charUpdate[i][2];
                 fighterName = charUpdate[i][4];
-                fightBaseAp =  charUpdate[i][5];
+                fightBaseAp =  charUpdate[i][5];               
             } 
         }
-
-        // defenderHp = parseInt($("#defenderBox").find("div").attr("value"));
-        // defenderConAtt = parseInt($("#defenderBox").find("div").attr("conAttack"));
-        // defenderName = $("#defenderBox").find(".characterName").text();
-        console.log("defender", defenderName);
-    
-        
-
+     
         defenderHp = defenderHp - fighterAttP;
         fighterHp = fighterHp- defenderConAtt;
-        //fighter's attack power increases each attack
-        fighterAttP += fightBaseAp;  
-
-        for (i=0;i<charUpdate.length;i++){
-    
-            if (charUpdate[i][0] === defenderId){
-                charUpdate[i][1] = defenderHp;
-            } else if (charUpdate[i][0] === fighterId){
-                charUpdate[i][1] = fighterHp;
-                charUpdate[i][2] = fighterAttP;
-            }
-        }
-        
+      
         if (fighterHp > 0){
             if (defenderHp <= 0){
                 removeDefender();
-                console.log("enemy number third",enemyNum);
+               
                 if (enemyNum > 0){
                     attackText = "You have defeated "+  defenderName + ". You can choose to fight another enemy";
                     selectDefFlag = true;
@@ -182,39 +169,78 @@ $(".attackBtn").on("click",function(){
             gameOverFlg = true;
         }
     
-        $("#attackMsg").find("span").text(attackText);
-        $("#attackMsg").find("p").text(conAttackText);
+        //fighter's attack power increases each attack
+        fighterAttP += fightBaseAp;  
+        //update charUpdate array to store the current health point, attackPower
+        for (i=0;i<charUpdate.length;i++){
+    
+            if (charUpdate[i][0] === defenderId){
+                charUpdate[i][1] = defenderHp;
+            } else if (charUpdate[i][0] === fighterId){
+                charUpdate[i][1] = fighterHp;
+                charUpdate[i][2] = fighterAttP;
+            }
+        }
         
-        $("#charBox").find("div").attr("attack",fighterAttP);
-        $("#charBox").find("span").text(fighterHp);
-        $("#defenderBox").find("span").text(defenderHp);
-   } else if ((!gameOverFlg)&&(defenderNum<=0)&&(selectDefFlag)){
-       attackText="No Enemy Here";
-       $("#attackMsg").find("span").text(attackText);
-       $("#attackMsg").find("p").text(conAttackText);
+        //Display attack and counter attack information, or defeated or win message
+        $("#attackMsg").find("span").text(attackText);
+        $("#attackMsg").find("p").text(conAttackText);        
+        //update health points
+        $("#myCharBox").find("span").text(fighterHp);
+        $("#defenderBox").find("span").text(defenderHp);        
+   } else 
+   // no defender and game not over and still have enemies to chose as defender, display message 
+       if ((!gameOverFlg)&&(defenderNum<=0)&&(selectDefFlag)){
+            attackText="No Enemy Here";
+            $("#attackMsg").find("span").text(attackText);
+            $("#attackMsg").find("p").text(conAttackText);
    }
+   //if gameOverFlg is true, do nothing.
 })
 
+//remove character under defenderBox
 function removeDefender(){
-    console.log("remove defender");
-    $("#defenderBox").find("div").detach().appendTo("#bkBox");
-    defenderNum--;
-    $("#bkBox").hide();
+    
+    $("#defenderBox").find("div").remove();
+    defenderNum--;    
 }
 
+//restart button onclick process.
 $(".restartBtn").on("click",function(){
-    console.log(" add restart logic");
-    // recreate the initial characters' layout, detach all imgCharBox1 to 4 from enemyBox and defenderBox
-    // append to charBox in the initial order
+   
+    // remove all characters from imgCharBox1 to 4 under myCharBox, enemyBox and defenderBox
+    $("#myCharBox").find("div").remove();
+    // for (i=0;i<enemyNum;i++){
 
-    $(".charBox").
-
+    // }
+    if (enemyNum > 0 ){
+        $("#enemyBox").find("div").remove();
+    }
+    if (defenderNum > 0 ){
+        $("#defenderBox").find("div").remove();
+    }
+    // show the original characters in charBox
+    $("#charBox").show();
+    $(".restartBtn").hide();
+    //clear the previous fighting message
+    $("#attackMsg").find("span").text("");
+    $("#attackMsg").find("p").text("");
     // initialize the flag variables
     firstSelectFlg = false;
     gameOverFlg = false;
     selectDefFlag = true;
     enemyNum = 0;
     defenderNum = 0;
+    fightBaseAp = 0;
+    fighterAttP = 0;
+    defenderConAtt = 0;
+    defenderHp = 0;
+    fighterHp = 0;
+    //reassign the value in array charUpdate from array charInitial
+    for (i=0;i<charInitial.length;i++){
+        charUpdate[i] = charInitial[i].slice();
+    }
+    
 })
 
 })
